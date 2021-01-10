53820a6ee3a53c176b4cfac46ad96df8d239ee8e
DexterLandry15 <56548223+DexterLandry15@users.noreply.github.com>
2021-01-10 22:33:18 +0300

    add template

--- routes/headlers.js
+++ routes/headlers.js
@@ -1,6 +1,27 @@
 const { Router } = require('express')
 const router = Router()
 
+const Gamedig = require('gamedig');
+
+let server_name = 'Unknown';
+let server_map = 'offline...';
+let server_players = 'NaN';
+
+Gamedig.query({
+    type: 'garrysmod',
+    host: '188.225.24.127'
+}).then((state) => {
+
+     server_name = state.name;
+     server_map = state.map;
+     server_players = `${state.players.length} / ${state.maxplayers}`;
+
+}).catch((error) => {
+    console.log("Server is offline");
+});
+
+
+
 const { token } = require('../config.json')
 const { VK } = require('vk-io');
 const vk = new VK({
@@ -14,6 +35,8 @@ async function vkapi(id) {
     });
     return photo_max;
 }
+
+
 
 
 
@@ -35,7 +58,10 @@ router.get('/servers', async (req, res) =>{
     res.render('servers', {
         title: 'CurlyDev | servers',
         style: 'servers.css',
-        isServers: true
+        isServers: true,
+        server_name: server_name,
+        server_map: server_map,
+        server_players: server_players
     })
 })
 
